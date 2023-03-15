4dd6b50c9f1c034d43f762df14df54b0b84d6447
retinderl <122463787+retinderl@users.noreply.github.com>
2023-03-15 16:21:01 -0400

    Generated by SqlDBM: Snowflake Demo - v144 by "retinder.labana@sqldbm.com"

--- SqlDBM/src/Tables/ADWORKS.DimCurrency.sql
+++ SqlDBM/src/Tables/ADWORKS.DimCurrency.sql
@@ -6,7 +6,7 @@
 CREATE TABLE ADWORKS.DimCurrency
 (
  CurrencyKey          number(38,0) NOT NULL AUTOINCREMENT START 1 INCREMENT 1 COMMENT 'Currency Identifier',
- CurrencyAlternateKey varchar(3) NOT NULL COMMENT 'Alternate Currency Identifier',
+ CurrencyAlternateKey dsf NOT NULL COMMENT 'Alternate Currency Identifier',
  CurrencyName22       varchar(50) NOT NULL COMMENT 'Name of the Currency',
 
  CONSTRAINT PK_DimCurrency PRIMARY KEY ( CurrencyKey )
