00c4398e854490ba084007a8724d338c0982c930
retinderl <122463787+retinderl@users.noreply.github.com>
2023-03-28 19:36:59 -0400

    Generated by SqlDBM: Snowflake Demo - v153 by "retinder.labana@sqldbm.com"

--- SqlDBM/src/Tables/table_35.sql
+++ SqlDBM/src/Tables/table_35.sql
@@ -3,7 +3,7 @@
 
 
 -- ************************************** table_35
-CREATE TABLE table_35
+CREATE OR REPLACE TABLE table_35
 (
  username varchar(50) NOT NULL, -- From template: "UserMeta"
  date     datetime NOT NULL, -- From template: "UserMeta"
